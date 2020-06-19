f2a25a651db0a88b8fbaf28e3d9c698a2a74dbe3
Vincent Bongartz <110112@st-maartenscollege.nl>
2020-06-19 06:54:17 +0000

    ik heb een def toegevoegd

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ Rotor1 = "uvwxyzabcdefghijklmnopqrst"
 Rotor2 = "nopqrstuvwxyzabcdefghijklm"
 Rotor3 = "ijklmnopqrstuvwxyzabcdefgh"
 
-#vul hieronder tussen de haakjes jouw gekozen woord in.
-Woord = str(input(""))
 
 print(alphabet.index("0"))
 print(Rotor1.index("0"))
+
+def encrypt(letter) :
+  ind = alphabet.index(letter)
+  newLetter = Rotor1[ind]
+  print(newLetter)
+
+  for letter in "woord":
+    encrypt letter
